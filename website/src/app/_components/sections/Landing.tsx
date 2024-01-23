a4142c56476b3aa7bb4496afdc98fbfe1a3a2895
Jeremy Bosma <jectaunscripted@gmail.com>
2024-01-23 09:03:37 +0100

    Update LandingSection component and remove react.svg

--- website/src/app/_components/sections/Landing.tsx
+++ website/src/app/_components/sections/Landing.tsx
@@ -1,20 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export function LandingSection() {
-    return <section>
-        <div className="cta">
+    const [operatingSystem, setOperatingSystem] = useState("");
 
-            <div className="headers">
-                <h1>One app. All your emulators.</h1>
-                <p className="subtext">Retrobox supports various emulators like Dolphin, Ryujinx & more.</p>
-                <p className="subtext">Available on Windows, MacOS & Linux</p>
+    useEffect(() => {
+        const userAgent = window.navigator.userAgent;
+        if (userAgent.includes("Win")) {
+            setOperatingSystem("Windows");
+        } else if (userAgent.includes("Linux")) {
+            setOperatingSystem("Linux");
+        } else {
+            setOperatingSystem("MacOS");
+        }
+    }, []);
+
+    return (
+        <section>
+            <div className="cta">
+
+                <div className="headers">
+                    <h1>One app. All your emulators.</h1>
+                    <p className="subtext">Retrobox supports various emulators like Dolphin, Ryujinx & more.</p>
+                    <p className="subtext">Available on Windows, MacOS & Linux</p>
+                </div>
+
+                <div className="buttons">
+                    <a className="downloadbutton" href="#">Download{operatingSystem && " for " + operatingSystem}</a>
+                    <a className="discord" href="#">Discord</a>
+                </div>
+
+                <Image
+                    src="/tv.png"
+                    className="tv"
+                    alt="placeholder"
+                    width={908}
+                    height={483}
+                />
             </div>
-
-            <div className="buttons">
-                <a className="downloadbutton" href="#">Download for MacOS</a>
-                <a className="discord" href="#">Discord</a>
-            </div>
-
-        </div>
-    </section>
-}
+        </section>
+    )
+};
